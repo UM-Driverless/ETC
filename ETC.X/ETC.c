4b18eb02f99f3e5bdda5214cd96e2a2639b799a9
93Urbano <davidjimenezurbano@gmail.com>
2021-12-24 17:57:19 +0100

    Añadido ETC.c

---
+++ ETC.X/ETC.c
@@ -0,0 +1,8 @@
+/* 
+ * File:   ETC.h
+ * Author: 93Urbano
+ *
+ * Created on December 24, 2021, 5:29 PM
+ */
+
+
---
+++ ETC.X/ETC.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ETC.h
+ * Author: 93Urbano
+ *
+ * Created on December 24, 2021, 5:29 PM
+ */
+
+#ifndef ETC_H
+#define	ETC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ETC_H */
+

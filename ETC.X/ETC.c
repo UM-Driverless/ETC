77a6db1e938506088fae5493b9afee6f597c3c3e
David Jimenez Urbano <david.jimenez1@tkelevator.com>
2022-06-01 10:21:10 +0200

    incluir PID de Rubén

--- ETC.X/ETC.c
+++ ETC.X/ETC.c
@@ -70,15 +70,15 @@ void APPSSend (unsigned char ucPercent)
 void APPSReadmin (void)
 {
     //Lo leido en sensores APPS se queda guardado como valor minimo
-    uiAPPS1min = uiAPPS1+APPSMARGEN;
-    uiAPPS2min = uiAPPS2-APPSMARGEN;
+    uiAPPS1min = uiAPPS1 + APPSMARGEN;
+    uiAPPS2min = uiAPPS2 - APPSMARGEN;
 }
 
 void APPSReadmax (void)
 {
     //Lo leido en sensores APPS se queda guardado como valor maximo
-    uiAPPS1max = APPS1max;
-    uiAPPS2max = APPS2max;
+    uiAPPS1max = APPS1max - APPSMARGEN;
+    uiAPPS2max = APPS2max + APPSMARGEN;
 }
 
 //Ejecutar cada vez que llegue el mensaje de modo autonomo
@@ -145,78 +145,91 @@ void ETCMove(unsigned char ucTargetMove, unsigned char ucMode)
     }
 }
 
-void ETC_PID(signed char scTargetMove, unsigned char ucMode) {
+void ETC_PID(signed long slTargetMove, unsigned char ucMode) 
+{
     /*
+     * Conditional logic to get target position out of this function, along with checks that it's ok to run it. PID just does PID.
+     * 
      * sl_target_perc is the target position of the throttle, in 0-100% = default-fully opened?
      * ucMode is the driving mode input to the function. It will be checked with the global variable 
      */
     
     // TODO REMOVE AND CALIBRATE VALUES, use PARAMETERS.h #define constants
     // Static local variables to change values at run time for calibration.
-    static signed long slIntegral = 0;
+    // Test with sl_target_perc ~= 80%, first K_I until it works with delay, then K_P to cancel those oscillations, then K_D to cancel the K_P oscillations.
+    static signed long sl_K = 50000; // Just to compensate the spring, which has approximately constant force, start with 50% of PWM duty cycle. The intake throttle is designed to go with the same force in both directions.
+    static signed long sl_K_P = 1000; // 2.1 starts to be unstable. 1.0 barely moves it. 1.5 is the right value. Multiply by divider value.
+    static signed long sl_K_I = 3; // 0.003 -> Small because it integrates.
+    static signed long sl_K_D = 0; // 20.0 to 200.0. If the error increases, you need to add power.
+    static signed long slIntegral = 0; // Usually in the range of 1e5 - 1e6
     signed long slDerivative;
-    signed long slPWMDuty = 0;
-    static signed long slLastErrorPos;
-    static signed long  slErrorPos;
+    signed long slMotorPwmDuty = 0;
+    static signed long  slErrorPos = 0; // Start as 0, then update every cycle.
+//    static signed long slLastErrorPos = 0;
+    static signed long slLastTPSPerc = 0;
+    
+    // Standard constants: K = 50 (%), K_P = 1.5, K_D = 20, K_I = 3
+    
+    // Position error:
+    slErrorPos = slTargetMove - ucTPS;  // in % or what units? TODO RJM ( (signed long)(uiTPS1_mv) - 1212 )*100 / (3126-1212)
+    
+    // Integral Error. Cap the values of the integral to avoid windup: https://en.wikipedia.org/wiki/Integral_windup, https://www.mstarlabs.com/apeng/techniques/pidsoftw.html
+    if ((slIntegral > -1e4) && (slIntegral < 1e4))
+    {
+        slIntegral += slErrorPos;
+    }
+    
+    // Derivative Error. Only make it work when descending? I don't feel like it does anything.
+    if (ucTPS < slLastTPSPerc)
+    {
+        slDerivative = slLastTPSPerc - ucTPS; // Behaves like friction. Ignores the input.
+    }
+    slLastTPSPerc = ucTPS; // For next iteration
+    
+    slMotorPwmDuty = sl_K + sl_K_P * slErrorPos + sl_K_I * slIntegral + sl_K_D * slDerivative;
+    Nop();
+    slMotorPwmDuty /= 1024; // 1024 ~= 1000. Do it because the constants are long, not floats
+
+    // Crop between 0 and 100. It's a duty cycle.
+    if (slMotorPwmDuty < 0) 
+    {
+        slMotorPwmDuty = 0;
+    }
+    else if ( slMotorPwmDuty > 100 ) 
+    {
+        slMotorPwmDuty = 100;
+    }
     
     Nop();
     
-    // Only let the motor move if CAN beat flag is OK. - TODO MOVE THESE CONDITIONALS TO THE MAIN.C FILE?
-    if ( ucETCFlagSupervisor == TRUE ) {
-        
-        /// MAIN CALCULATION
-        
-        // ui_tp1_mv is the current position. Usually 16-55 for default-open.
-        
-        // TODO ---- USE ucTPS_perc INSTEAD OF uiTPS1_mv
-        // TODO: 1. play sound of ucTPS_perc to check. 2. Use ucTPS_perc INSTEAD OF uiTPS1_mv
-        //slErrorPos = (slTargetMove) - ( (signed long)(uiTPS1) - 1212 )*100 / (3126-1212);  // in % or what units? TODO RJM
-        slErrorPos = scTargetMove - ucTPS;  // in % or what units? TODO RJM
-        slIntegral += slErrorPos;
-        slDerivative = slErrorPos - slLastErrorPos;
-        
-        // Bound the values of slIntegral - TODO bounds to PARAMETERS.h, or remove if not needed.
-//        if (slIntegral > (1<<20)){
-//            slIntegral = 1<<20;
-//        } else if (slIntegral < -32768){
-//            slIntegral = 0;
-//        }
-        
-        slPWMDuty = sl_K + sl_K_P * slErrorPos + sl_K_I * slIntegral + sl_K_D * slDerivative; // TODO complete
-        slPWMDuty /= 1000; // Because the constants are long, not floats        
-        
-        // Crop between 0 and 100. It's a duty cycle.
-        if ( slPWMDuty < 0 ) {
-            slPWMDuty = 0;
-        }
-        else if ( slPWMDuty > 100 ) {
-            slPWMDuty = 100;
-        }
-        
-        Nop();
-        
-        // MAIN MOVEMENT - DEPENDS ON ASMode - TODO remove conditionals to make it faster?
-        if ( ucMode == ucASMode ) {
-            if ( ucASMode == ASMode ) {
-                //ETCMove(sl_motor_pwm_duty, ASMode); // TODO UNCOMMENT AND MAKE IT WORK
-                GPIO_PWM2_Control(slPWMDuty, 600);
+    // APPLY THE DUTY CYCLE TO THE MOTOR - Variable checks.
+    // Only let the motor move if CAN beat flag is OK
+    // TODO Consider doing this externally, and giving a value to PID function depending on state. PID just moves to target position. Otherwise we need several target positions into the function.
+    if ( ucETCFlagSupervisor == TRUE ) // OK no errors...
+    { 
+        if ( ucMode == ucASMode ) // The mode sent to the PID function and the global variable agree
+        { 
+            if (ucASMode == ASMode)
+            {
+                // Drive with autonomous value
+                GPIO_PWM2_Control(slMotorPwmDuty, 600);
+            } 
+            else if (ucASMode == ManualMode)
+            {
+                GPIO_PWM2_Control(slMotorPwmDuty, 600); // Usually this
+            } 
+            else 
+            {
+                // ERROR. Wrong value of ucASMode
             }
-            else if ( ucASMode == ManualMode ) {
-                //ETCMove(sl_motor_pwm_duty, ASMode); // TODO UNCOMMENT AND MAKE IT WORK
-//                ETCMove(sl_motor_pwm_duty, ManualMode);
-                GPIO_PWM2_Control(slPWMDuty, 600);
-            }
-            else {
-                // ERROR: Wrong value
-            }
-        } else {
-            // ERROR: Movement prevented by driving mode
-        }
-        
-         // Set error as last error for next iteration
-        slLastErrorPos = slErrorPos;
-    } else {
-        // ET Motor OFF
+        } 
+        else 
+        {
+            // ERROR. Movement prevented by driving mode
+        } 
+    } 
+    else 
+    {
         GPIO_PWM2_Control(0, 600);
     }
 }
@@ -266,47 +279,31 @@ void ETCCalibrate(void) {
 void TPSAnalysis(void)
 {
     //Analisis TPS1
-    if ( uiTPS1min < uiTPS1max )    //TPS1 voltaje no invertido
+    /*if ( uiTPS1min < uiTPS1max )    //TPS1 voltaje no invertido
     {
-        //ulTPS1calc = ( ( uiTPS1max - uiTPS1min ) * uiETCDuty ) + uiTPS1min;
-        /*ulTPS1calc = ( uiTPS1max - uiTPS1min );
-        ulTPS1calc = ( ulTPS1calc * uiETCDuty );
-        ulTPS1calc = ( ( ulTPS1calc / 100 ) + uiTPS1min );*/
         ucTPS1Perc = ETCPercentCalc (uiTPS1, uiTPS1max, uiTPS1min);
         ucTPS_Volts_STATE = TPS1_NO_INVERTED;
     }
     else    //TPS1 voltaje invertido
     {
-        //ulTPS1calc = ( uiTPS1min - ( ( uiTPS1min - uiTPS1max ) * uiETCDuty ) );
-        /*ulTPS1calc = ( uiTPS1min - uiTPS1max );
-        ulTPS1calc = ( ulTPS1calc * uiETCDuty );
-        ulTPS1calc = ( ( uiTPS1min * 100 ) - ulTPS1calc );*/
         ucTPS1Perc = 100-ETCPercentCalc (uiTPS1, uiTPS1min, uiTPS1max);
         ucTPS_Volts_STATE = TPS1_INVERTED;
     }
     
     if ( uiTPS2min < uiTPS2max )    //TPS2 voltaje no invertido
     {
-        //ulTPS2calc = ( ( uiTPS2max - uiTPS2min ) * uiETCDuty ) + uiTPS2min;
-        /*ulTPS2calc = ( uiTPS2max - uiTPS2min );
-        ulTPS2calc = ( ulTPS2calc * uiETCDuty );
-        ulTPS2calc = ( ( ulTPS2calc / 100 ) + uiTPS2min );*/
         ucTPS2Perc = ETCPercentCalc (uiTPS2, uiTPS2max, uiTPS2min);
         ucTPS_Volts_STATE = TPS2_NO_INVERTED;
     }
     else    //TPS2 voltaje invertido
     {
-        //ulTPS2calc = ( uiTPS2min - ( ( uiTPS2min - uiTPS2max ) * uiETCDuty ) );
-        /*ulTPS2calc = ( uiTPS2min - uiTPS2max );
-        ulTPS2calc = ( ulTPS2calc * uiETCDuty );
-        ulTPS2calc = ( ( uiTPS2min * 100 ) - ulTPS2calc );*/
         ucTPS2Perc = 100-ETCPercentCalc (uiTPS2, uiTPS2max, uiTPS2min);
         ucTPS_Volts_STATE = TPS2_INVERTED;
-    }
+    }*/
     
     
-    //ucTPS1Perc = ( ulTPS1calc & 0x00007F );
-    //ucTPS2Perc = ( ulTPS2calc & 0x00007F );
+    ucTPS1Perc = ETCPercentCalc (uiTPS1, uiTPS1max, uiTPS1min);
+    ucTPS2Perc = ETCPercentCalc (uiTPS2, uiTPS2max, uiTPS2min);
     ucTPS = ( ( ucTPS1Perc + ucTPS2Perc ) / 2 );
     Nop();
     
@@ -369,49 +366,26 @@ void TPSAnalysis(void)
 void APPSAnalysis (void)
 {
     //Analisis APPS1
-    Nop();
-    __delay_ms(50);
-    Nop();
-    Nop();
-    Nop();
-    if ( uiAPPS1min < uiAPPS1max )    //APPS1 voltaje no invertido
+    /*if ( uiAPPS1min < uiAPPS1max )    //APPS1 voltaje no invertido
     {
-        //DEBERIAMOS SACAR ERROR PORQUE EL CALCULO SALE MAL POR LOS LIMITES
-        //ulAPPS1calc = ((uiAPPS1-uiAPPS1min)/(uiAPPS1min-uiAPPS1max))*100;
-        /*ulAPPS1calc = (uiAPPS1-uiAPPS1min);
-        ulAPPS1calc = ulAPPS1calc*100;
-        ulAPPS1calc = (ulAPPS1calc/(uiAPPS1min-uiAPPS1max));*/
         ucAPPS1Perc = ETCPercentCalc (uiAPPS1, uiAPPS1min, uiAPPS1max);
     }
     else    //APPS1 voltaje invertido
     {
-        //ulAPPS1calc = ((uiAPPS1min-uiAPPS1)/(uiAPPS1min-uiAPPS1max))*100;
-        /*ulAPPS1calc = (uiAPPS1min-uiAPPS1);
-        ulAPPS1calc = ulAPPS1calc*100;
-        ulAPPS1calc = (ulAPPS1calc/(uiAPPS1min-uiAPPS1max));*/
         ucAPPS1Perc = 100-ETCPercentCalc (uiAPPS1, uiAPPS1max, uiAPPS1min);
     }
     //Analisis APPS2
     if ( uiAPPS2min < uiAPPS2max )    //APPS2 voltaje no invertido
     {
-        //ulAPPS2calc = ((uiAPPS2-uiAPPS2min)/(uiAPPS2max-uiAPPS2min))*100;
-        /*ulAPPS2calc = (uiAPPS2-uiAPPS2min);
-        ulAPPS2calc = ulAPPS2calc*100;
-        ulAPPS2calc = (ulAPPS2calc/(uiAPPS2max-uiAPPS2min));*/
         ucAPPS2Perc = 100-ETCPercentCalc (uiAPPS2, uiAPPS2max, uiAPPS2min);
     }
     else    //APPS2 voltaje invertido
     {
-        //DEBERIAMOS SACAR ERROR PORQUE EL CALCULO SALE MAL POR LOS LIMITES
-        //ulAPPS2calc = ((uiAPPS2min-uiAPPS2)/(uiAPPS2min-uiAPPS2max))*100;
-        /*ulAPPS2calc = (uiAPPS2min-uiAPPS2);
-        ulAPPS2calc = ulAPPS2calc*100;
-        ulAPPS2calc = (ulAPPS2calc/(uiAPPS2min-uiAPPS2max));*/
         ucAPPS2Perc = ETCPercentCalc (uiAPPS2, uiAPPS2min, uiAPPS2max);
-    }
+    }*/
     
-    //ucAPPS1Perc = ( ulAPPS1calc & 0x00007F );
-    //ucAPPS2Perc = ( ulAPPS2calc & 0x00007F );
+    ucAPPS1Perc = ETCPercentCalc (uiAPPS1, uiAPPS1max, uiAPPS1min);
+    ucAPPS2Perc = ETCPercentCalc (uiAPPS2, uiAPPS2max, uiAPPS2min);
     ucAPPS = ( ( ucAPPS1Perc + ucAPPS2Perc ) / 2 );
     Nop();
 }
@@ -452,20 +426,23 @@ void ETCManual (unsigned char ucTargetManual)
 }
 
 //Declaracion funciones
-unsigned char ETCPercentCalc(signed long val, signed long min, signed long max) {
-    /*
-     * Returns the percentage of val between min and max. Result bounded between 0 and 100.
-     * 
+unsigned char ETCPercentCalc(signed long val, signed long min, signed long max) 
+{
+    /*Returns the percentage of val between min and max. Result bounded between 0 and 100.
      * If val is out of the range [min,max], then it will be capped at 0% or 100%.
      * If min is greater than max, the closest val is to min, the closest the result will be to 0%, and viceversa.
      * If val exceeds the range going through min, it will be 0%. It if goes through max, it will be 100%, even if min > max.
      * A higher value will be 0% in this case.
      */
     val = 100*(val - min)/(max - min);
-    if (val < 0){
+    if (val < 0)
+    {
         val = 0;
-    } else if (val > 100 ){
+    } 
+    else if (val > 100 )
+    {
         val = 100;
-    } // val is now contained in the interval [0,100] -> ok to assign to unsigned char
+    } 
+    // val is now contained in the interval [0,100] -> ok to assign to unsigned char
     return val;
 }
